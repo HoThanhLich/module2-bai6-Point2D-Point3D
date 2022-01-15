3f703a016570f81999f9bdbf1f3220802596f530
HoThanhLich <thanhlichpvmtc@gmail.com>
2022-01-15 21:24:02 +0700

    them file

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    Point2D point2D = new Point2D();
+//        System.out.println(point2D);
+//
+//        System.out.println("---------");
+//        point2D = new Point2D(5,8);
+//        System.out.println(point2D);
+//
+//        System.out.println("----------");
+
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+        System.out.println("**********");
+
+        point3D = new Point3D(1.5f,2.3f,5);
+        System.out.println(point3D);
+        System.out.println("*****");
+
+    }
+}
+
